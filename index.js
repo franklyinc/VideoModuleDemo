043bc8af36e0c5e721d152eb135143dd82ed471b
Ehrens, Matthew <mehrens@franklyinc.com>
2018-08-03 17:41:24 -0400

    21 full wnVideoCanvasProps

--- index.js
+++ index.js
@@ -37,14 +37,35 @@ class VideoModuleDemo extends Component {
   updateVideo() {
     this.setState({
       id: 2,
-      wNVideoCanvasProps[0].SetFlashLiveStream[0].mobileStreams[0].url: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
+
+      wNVideoCanvasProps: [{
+        SetFlashLiveStream: [{
+            strUrl: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+            strHeadline: 'Video Test 2',
+            strSummaryImageUrl: '',
+            mobileStreamCcUrl: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+            hasPreroll: true,
+            strAdTag: 'Weather - Live Streaming',
+            mobileStreams: [{
+              url: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+              type: 'video/mp4'
+            }, {
+              url: '',
+              type: 'video/mp4'
+            }, {
+              url: '',
+              type: 'video/mp4'
+            }] // mobileStreams
+          }] // SetFlashLiveStream
+      }] // wNVideoCanvasProps
+      
     });
   }
   
   render(){
     return (
       <div className='VideoModuleDemo'>	
-        <h1>Video Demo 20 - {this.state.id}</h1> <button onClick={this.updateVideo}>Video2</button>
+        <h1>Video Demo 21 - {this.state.id}</h1> <button onClick={this.updateVideo}>Video2</button>
 	      <Video clipId={this.state.id} autoPlay={this.state.autoPlay} wNVideoCanvasProps={this.state.wNVideoCanvasProps} />
       </div>
     );
