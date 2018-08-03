5afade3e0a8611c305621487c8b8f1372cbfab65
Ehrens, Matthew <mehrens@franklyinc.com>
2018-08-03 17:58:11 -0400

    24 cDM

--- index.js
+++ index.js
@@ -10,72 +10,74 @@ class VideoModuleDemo extends Component {
       video: ''
     }; // this.state
     
-    this.updateVideo = this.updateVideo.bind(this);
+    // this.updateVideo = this.updateVideo.bind(this);
 
   } // constructor
 
   componentDidMount() {
     const id = 1;
     const autoPlay = true;
+    const url = 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8';
+    const headline = 'Video Test 2';
+    const liveStreamInfo = {
+      strUrl: url,
+      strHeadline: headline,
+      strSummaryImageUrl: '',
+      mobileStreamCcUrl: url,
+      hasPreroll: true,
+      strAdTag: 'Weather - Live Streaming',
+      mobileStreams: [{
+        url,
+        type: 'video/mp4'
+      }, {
+        url: '',
+        type: 'video/mp4'
+      }, {
+        url: '',
+        type: 'video/mp4'
+      }]
+    };
     const wNVideoCanvasProps = [{
-        SetFlashLiveStream: [{
-          strUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-          strHeadline: headline,
-          strSummaryImageUrl: '',
-          mobileStreamCcUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-          hasPreroll: true,
-          strAdTag: 'Weather - Live Streaming',
-          mobileStreams: [{
-            url: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-            type: 'video/mp4'
-          }, {
-            url: '',
-            type: 'video/mp4'
-          }, {
-            url: '',
-            type: 'video/mp4'
-          }] // mobileStreams
-        }] // SetFlashLiveStream
-      }]; // wNVideoCanvasProps
-      this.setState({
-        id,
-        autoplay,
-        video: <Video clipId={id} autoPlay={autoPlay} wNVideoCanvasProps={wNVideoCanvasProps} />
-      });
-  }
-  
-  updateVideo() {
-    this.setState({
-      id: 2,
-
-      wNVideoCanvasProps: [{
-        SetFlashLiveStream: [{
-            strUrl: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
-            strHeadline: 'Video Test 2',
-            strSummaryImageUrl: '',
-            mobileStreamCcUrl: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
-            hasPreroll: true,
-            strAdTag: 'Weather - Live Streaming',
-            mobileStreams: [{
-              url: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
-              type: 'video/mp4'
-            }, {
-              url: '',
-              type: 'video/mp4'
-            }, {
-              url: '',
-              type: 'video/mp4'
-            }] // mobileStreams
-          }] // SetFlashLiveStream
-      }] // wNVideoCanvasProps
+      SetFlashLiveStream: [liveStreamInfo]
+    }];
       
+    this.setState({
+      video: <Video clipId={id} autoPlay={autoPlay} wNVideoCanvasProps={wNVideoCanvasProps} />
     });
   }
+  
+  // updateVideo() {
+  //   this.setState({
+  //     id: 2,
+  // 
+  //     wNVideoCanvasProps: [{
+  //       SetFlashLiveStream: [{
+  //           strUrl: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+  //           strHeadline: 'Video Test 2',
+  //           strSummaryImageUrl: '',
+  //           mobileStreamCcUrl: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+  //           hasPreroll: true,
+  //           strAdTag: 'Weather - Live Streaming',
+  //           mobileStreams: [{
+  //             url: 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+  //             type: 'video/mp4'
+  //           }, {
+  //             url: '',
+  //             type: 'video/mp4'
+  //           }, {
+  //             url: '',
+  //             type: 'video/mp4'
+  //           }] // mobileStreams
+  //         }] // SetFlashLiveStream
+  //     }] // wNVideoCanvasProps
+  // 
+  //   });
+  // }
   
   render(){
     return (
       <div className='VideoModuleDemo'>	
-        <h1>Video Demo 23 - {this.state.id}</h1> <button onClick={this.updateVideo}>Video2</button>
+        <h1>Video Demo 24 - {this.state.id}</h1> 
 	      {this.state.video}
       </div>
     );
