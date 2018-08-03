c8ffa5ee0936faf347d9ad598016cdaf74043b31
Ehrens, Matthew <mehrens@franklyinc.com>
2018-08-03 17:06:23 -0400

    revert

--- index.js
+++ index.js
@@ -4,40 +4,39 @@ import Video from 'components/media/Video';
 class VideoModuleDemo extends Component {
   constructor(props) {
     super(props);
-	  
     this.state = {
+      
       id: 1,
       autoplay: true,
       wNVideoCanvasProps: [{
-          SetFlashLiveStream: [{
-              strUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-              strHeadline: 'Encoder 2 Test CBS Stream',
-              strSummaryImageUrl: '',
-              mobileStreamCcUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-              hasPreroll: true,
-              strAdTag: 'Weather - Live Streaming',
-              mobileStreams: [{
-                url: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-                type: 'video/mp4'
-              }, {
-                url: '',
-                type: 'video/mp4'
-              }, {
-                url: '',
-                type: 'video/mp4'
-              }] // mobileStreams
-            }] // SetFlashLiveStream
-        }] // wNVideoCanvasProps
+        SetFlashLiveStream: [{
+            strUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
+            strHeadline: 'Encoder 2 Test CBS Stream',
+            strSummaryImageUrl: '',
+            mobileStreamCcUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
+            hasPreroll: true,
+            strAdTag: 'Weather - Live Streaming',
+            mobileStreams: [{
+              url: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
+              type: 'video/mp4'
+            }, {
+              url: '',
+              type: 'video/mp4'
+            }, {
+              url: '',
+              type: 'video/mp4'
+            }] // mobileStreams
+          }] // SetFlashLiveStream
+      }] // wNVideoCanvasProps
     }; // this.state
     
   } // constructor
   
   render(){
-    
     return (
       <div className='VideoModuleDemo'>	
-        <h1>Video Demo 7</h1>
-	      <Video autoPlay={this.state.autoPlay} wNVideoCanvasProps={this.state.wNVideoCanvasProps} />
+        <h1>Video Demo 8</h1>
+	      <Video clipId={this.state.id} autoPlay={this.state.autoPlay} wNVideoCanvasProps={this.state.wNVideoCanvasProps} />
       </div>
     );
 	  
