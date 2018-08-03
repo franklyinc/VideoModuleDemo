9de177a999381ce1e1122049c31fd47c0e8cb943
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-08-03 16:19:57 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+import React, { Component, PropTypes } from 'react';
+import Video from 'components/Video';
+
+class VideoModuleDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+      id: 1,
+      autoplay: true,
+      wNVideoCanvasProps: [{
+        SetFlashLiveStream: [{
+            strUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
+            strHeadline: 'Encoder 2 Test CBS Stream',
+            strSummaryImageUrl: '',
+            mobileStreamCcUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
+            hasPreroll: true,
+            strAdTag 'Weather - Live Streaming': ,
+            mobileStreams: [{
+              url: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
+              type: 'video/mp4'
+            }, {
+              url: '',
+              type: 'video/mp4'
+            }, {
+              url: '',
+              type: 'video/mp4'
+            }] // mobileStreams
+          }] // SetFlashLiveStream
+      }]; // wNVideoCanvasProps
+    }; // this.state
+    
+  } // constructor
+  
+  render(){
+    return (
+      <div className='VideoModuleDemo'>	
+        <h1>Video Demo</h1>
+	      <Video clipId={this.state.id} autoPlay={this.state.autoPlay} wNVideoCanvasProps={this.state.wNVideoCanvasProps} />
+      </div>
+    );
+	  
+  }
+}
+
+export default VideoModuleDemo;
