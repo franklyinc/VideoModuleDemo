5a33f029d365757096432870bcc7a79f3124f4bf
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-08-15 09:20:03 -0400

    remove equals

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import Video from 'components/media/Video';
-import axios = from 'axios';
+import axios from 'axios';
 
 class VideoModuleDemo extends Component {
   constructor(props) {
