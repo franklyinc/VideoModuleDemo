e0c2fe80763fbfc4f843cc8bdaf59ed6f66ae8ba
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-08-17 18:14:52 -0400

    number8

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Video from 'components/media/Video';
 
 class VideoModuleDemo extends Component {
 
@@ -6,7 +7,7 @@ class VideoModuleDemo extends Component {
 
     return (
       <div className='VideoModuleDemo'>	
-        <h1>Video Demo 7 </h1>
+        <h1>Video Demo 8</h1>
       </div>
     );	 
 	  
