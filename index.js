57009bf938ebff09c3b0579ee0a77703d7a130ae
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2019-03-25 17:52:38 -0400

    bbb

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class VideoModuleDemo extends Component {
     this.videoPlaylist = [
       'https://channels-1.video.franklyinc.com/out/i/15813.m3u8',
 	    'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
-	    'http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8'
+	    'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8'
     ]
 
   }
