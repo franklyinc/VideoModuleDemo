5c45673cb110a23f4f954b29fbd57c7bc5ade54f
ME <mehrens+gitkracken@gmail.com>
2019-06-02 14:59:02 -0400

    WEB-5684 testing 2

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import Video from 'components/media/Video';
 class VideoModuleDemo extends Component {
   static defaultProps = {
     "titles": [
-      // "Vid1",
-      // "Vid2"
+      "Stream 1",
+      "Stream 2"
     ],
     "streamUrls": [
-      // "https://channels-1.video.franklyinc.com/out/i/15812.m3u8",
-      // "https://channels-1.video.franklyinc.com/out/i/19735.m3u8"
+      "https://channels-1.video.franklyinc.com/out/i/15812.m3u8",
+      "https://channels-1.video.franklyinc.com/out/i/19735.m3u8"
     ],
     "posterUrls": [
-      // "https://FRANKLYDEMO.images.worldnow.com/images/18020840_G.jpeg",
-      // "https://FRANKLYDEMO.images.worldnow.com/images/18018385_G.jpeg"
+      "https://FRANKLYDEMO.images.worldnow.com/images/18020840_G.jpeg",
+      "https://FRANKLYDEMO.images.worldnow.com/images/18018385_G.jpeg"
     ]
   }
 
