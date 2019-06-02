dd4b2be2d7bcd519ca2e15769b24ed2db9e1c647
ME <mehrens+gitkracken@gmail.com>
2019-06-02 14:36:43 -0400

    WEB-5684 template cols

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ class VideoModuleDemo extends Component {
       width: '100%',
       height: '100%',
       gridTemplateAreas: "", // will be dynamically generated below in streamUrls map
-      gridTemplateColumns: 'auto-fit 180px',
+      gridTemplateColumns: '75% 25%',
       gridTemplateRows: "", // will be dynamically generated below in streamUrls map
       gridRowGap: '16px',
       rowGap: '16px'
